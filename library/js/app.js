c5a1462b8a1a9d072d95b722feabc1152a15a756
AntonTsekhovy <toxats89@gmail.com>
2023-08-07 18:53:50 +0300

    feat: adaptive header

--- library/js/app.js
+++ library/js/app.js
@@ -1,3 +1,22 @@
+const burger = document.querySelector(".header__burger");
+const list = document.querySelector(".header__list");
+
+const toggleMenu = () => {
+  burger.classList.toggle("header__burger--active");
+  list.classList.toggle("header__list--active");
+};
+const closeMenu = () => {
+  burger.classList.remove("header__burger--active");
+  list.classList.remove("header__list--active");
+};
+document.addEventListener("click", (e) => {
+  const click = e.composedPath().includes(burger);
+  if (click) {
+    toggleMenu();
+  } else {
+    closeMenu();
+  }
+});
 console.log(
   { "Вёрстка валидная": 10 },
   "\n",
